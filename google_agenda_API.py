c7479afcedd651b9768fc9176adc6baee263efac
Matthiazz123 <matthias.pattijn@hotmail.com>
2020-11-06 20:39:26 +0100

    main updated

--- google_agenda_API.py
+++ google_agenda_API.py
@@ -2,31 +2,32 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 
-PATH = "chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-driver.get("https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Ad0ba58f3907446ec%2C10%3A1602440566%2C16%3A5e8fa0befc74fda1%2C0854e629b691884eaa22bc7bc4d1d9b3f4296853ac762b201e8e3403bbfa01f7%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22aef0a6b6f41e46f493cc26b144abd02e%22%7D&response_type=code&flowName=GeneralOAuthFlow")
 
-def login_google(email, pasw):
-	email_form = driver.find_element_by_xpath('//*[@id="identifierId"]')
-	email_form.send_keys(email)
-	next_button = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]')
-	next_button.click()
-	time.sleep(2)
-	pasw_form = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input')
-	pasw_form.send_keys(pasw)
-	login_button = driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]')
-	login_button.click()
-	driver.implicitly_wait(5)
-	time.sleep(2)
 
-def add_task(task, date, discr):
+def login_google(driver, email, pasw):
+        driver.get("https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Ad0ba58f3907446ec%2C10%3A1602440566%2C16%3A5e8fa0befc74fda1%2C0854e629b691884eaa22bc7bc4d1d9b3f4296853ac762b201e8e3403bbfa01f7%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22aef0a6b6f41e46f493cc26b144abd02e%22%7D&response_type=code&flowName=GeneralOAuthFlow")
+        email_form = driver.find_element_by_xpath('//*[@id="identifierId"]')
+        email_form.send_keys(email)
+        next_button = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]')
+        next_button.click()
+        time.sleep(2)
+        pasw_form = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input')
+        pasw_form.send_keys(pasw)
+        login_button = driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]')
+        login_button.click()
+        driver.implicitly_wait(5)
+        time.sleep(2)
+        driver.get("https://calendar.google.com/calendar/u/0/r/agenda")
+        time.sleep(3)
+
+def add_task(driver, task, date, discr):
         add_button = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div[1]/button/span[2]/div[2]')
         add_button.click()
         time.sleep(1)
         task_button = driver.find_element_by_xpath('//*[@id="yDmH0d"]/div/div/div[2]/span/div/div[1]/div[3]/div[1]/div[2]/div[1]/div[3]/span/span')
         task_button.click()
         task_form = driver.find_element_by_xpath('//*[@id="yDmH0d"]/div/div/div[2]/span/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/div/div[1]/input')
-        task_form.send_keys('Taak: ' + task)
+        task_form.send_keys(task)
         time.sleep(1)
         date_form = driver.find_element_by_xpath('//*[@id="xStDaIn"]')
         date_form.send_keys(Keys.CONTROL + 'a')
@@ -42,7 +43,7 @@ def add_task(task, date, discr):
         save_button.click()
         time.sleep(1)
 
-def add_SB(SB, date, discr):
+def add_SB(driver, SB, date, discr):
         #add event
         add_button = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div[1]/button/span[2]/div[2]')
         add_button.click()
@@ -60,16 +61,17 @@ def add_SB(SB, date, discr):
         whole_day.click()
         #goto event (date is the same)
         event_button = driver.find_element_by_xpath('//*[@id="yDmH0d"]/div/div/div[2]/span/div/div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/span')
-        event_button.click()                         
+        event_button.click()
+        time.sleep(1)
         #change the agenda you save in
         change_button = driver.find_element_by_xpath('//*[@id="tabEvent"]/div/div[7]/div[1]/div/div[2]/div/div/span/span/div[1]/div')
         change_button.click()
         time.sleep(1)
-        change2_button = driver.find_element_by_xpath('//*[@id="xCalSel"]/div[1]/div[1]/div[1]/span/div/div')
+        change2_button = driver.find_element_by_xpath('//*[@id="xCalSel"]/div[1]/div[1]/div[2]')
         change2_button.click()
         time.sleep(1)
         for i in range(2, 6): #look for agenda called SB's
-                button = driver.find_element_by_xpath(f'//*[@id="xCalSel"]/div[2]/div[{i}]/span/div/div')
+                button = driver.find_element_by_xpath(f'//*[@id="xCalSel"]/div[1]/div[1]/div[2]')
                 if button.get_attribute("innerHTML") == "SB's":
                         button.click() 
                         break                  
@@ -81,22 +83,11 @@ def add_SB(SB, date, discr):
         discr_button = driver.find_element_by_xpath('//*[@id="tabEvent"]/div/div[6]/div[1]/div/div[2]/div/div/span')
         discr_button.click()
         time.sleep(1)
-        discr_form = driver.find_element_by_xpath('//*[@id="T2Ybvb2"]')
+        discr_form = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/span/div/div[1]/div[3]/div[1]/div[2]/div[2]/span[1]/div/div[6]/div[2]/div/div[1]/div[2]/div/div[2]/div/div/div[1]/div[2]/div[2]')
         discr_form.send_keys(discr)
         time.sleep(1)
         #save the event
         save_button = driver.find_element_by_xpath('//*[@id="yDmH0d"]/div/div/div[2]/span/div/div[1]/div[3]/div[2]/div[2]/span')
-        save_button.click()    
-        
-'''     
-login_google("matthiashenno2004", "Sissen1957")
-driver.get("https://calendar.google.com/calendar/u/0/r/agenda")
-time.sleep(3)
-add_task("test","22 10 20", "Dit is een test")
-add_SB("test", "22 10 20", "Dit is een test")
-'''
+        save_button.click()
+        time.sleep(1)
 
-login_google("cuyversarno", "kerkStraat35")
-driver.get("https://calendar.google.com/calendar/u/0/r/agenda")
-time.sleep(3)
-add_SB("test","22 10 20", "Dit is een test")
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from selenium import webdriver
 from smartschool_tasks_API import *
+from google_agenda_API import *
+
+def read_json(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+def write_json(filename, new_data):
+    with open(filename) as f:
+        json.dump(new_data, f)
 
 PATH = "chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -7,4 +16,35 @@ driver = webdriver.Chrome(PATH)
 login(driver, "CyvrsAr", "dVKpfwz69gVprMeG")
 lol = convertTasks(getTasks(driver))
 print(lol)
+login_google(driver, "matthiashenno2004", "Sissen1957")
+print(len(lol))
+
+for i in range(len(lol)):
+    if 'Taak' in lol[i]:
+        print('Taak')
+        dag, taak, vak, discr = lol[i]
+        print(lol[i])
+        if taak +": " + vak in driver.page_source:
+            print("skipped")
+            print("-----------------------------")
+        else:
+            add_task(driver, taak +': '+ vak, dag,discr)
+            print("done")
+            print("-----------------------------")
+                
+    elif 'Toets' in lol[i]:
+        print('SB/Toets:')
+        dag, sb, vak, discr = lol[i]
+        print(lol[i])
+        if sb +": " + vak in driver.page_source:
+            print("skipped")
+            print("-----------------------------")
+        else:
+            add_SB(driver, sb +': '+ vak, dag,discr)
+            print("done")
+            print("-----------------------------")
+            
+
+
+
 
